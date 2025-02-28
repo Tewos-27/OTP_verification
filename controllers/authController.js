443bd079c0e4693cdd4e2dd110c82a9c2264e270
Tewos-21 <tewodrosshimels54@gmail.com>
2025-02-28 04:32:53 -0800

    Add aemail transport setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,8 +7,8 @@ const crypto = require('crypto');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: '',
-        pass: ''
+        user: 'tewodrosshimels268@gmail.com',
+        pass: 'Tewo@1921'
     }
 })
 
@@ -17,4 +17,25 @@ const transporter = nodemailer.createTransport({
 const generateOTP = () => crypto.randomInt(100000, 999999).toString();
 
 // register User and send Otp
+
+exports.register = async (req, res) => {
+    try{
+
+        const { name, email, password } =req.body;
+        let user = await User.findOne({ email });
+
+        if (user) return res.status(400).json({ message: 'User already exists'});
+        
+        const otp = generateOTP();
+        const otpExpiry = new Date(Date.now() + 10 * 60 * 1000);
+
+        user = new User({ name, email, password, otp, otpExpiry});
+
+        await user.save();
+
+        await transporter.sendMail({
+            from: ''
+        })
+    }
+}
 
