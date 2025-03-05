7937d2b68a660129de51321e6a4493358cede7ca
Tewos-21 <tewodrosshimels54@gmail.com>
2025-03-05 03:08:38 -0800

    add some feature

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ connectDB();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
 
+app.use(session({
+    secret: 'supersecretkey',
+    resave: false,
+    saveUninitilized: true,
+    cookie: {secure: false}
+}));
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 const PORT = 3000;
 app.listen(PORT, () => 
     console.log('Server running on port ${PORT}')
