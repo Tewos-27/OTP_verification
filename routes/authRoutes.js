48ce80b40107e8d68e3dc2301d44d9b67be7707d
Tewos-21 <tewodrosshimels54@gmail.com>
2025-04-29 03:32:16 -0700

    update changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const { register, verifyOTP, resendOTP, login, logout, dashboard} = require('../controllers/authController');
 const authMiddleware = require('../middleware/authmiddleware');
