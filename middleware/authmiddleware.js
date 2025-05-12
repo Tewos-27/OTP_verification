9876ca5f65bd2cdc4719a9f0da4fbf64ae43ad02
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-11 22:54:37 -0700

    update all changes

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,3 +1,4 @@
+// middleware thst proceess the request before it reaches the controller
 module.exports = (req, res, next) => {
     if (!req.session.user){
         return res.status(401).json({ message: "Unauthorized, Please log om first"});
