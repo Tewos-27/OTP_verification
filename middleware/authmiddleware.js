ba62cbb1245032a9e12ba3d5cda2fcca7f389e52
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-12 00:53:32 -0700

    update all changes

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,4 +1,8 @@
 // middleware thst proceess the request before it reaches the controller
+// middleware that checks if the user is authenticated
+// This middleware checks if the user is authenticated by checking if the user object exists in the session
+// If the user is not authenticated, it sends a 401 Unauthorized response
+// If the user is authenticated, it calls the next middleware or route handlerS
 module.exports = (req, res, next) => {
     if (!req.session.user){
         return res.status(401).json({ message: "Unauthorized, Please log om first"});
