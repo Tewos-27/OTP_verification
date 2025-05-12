7700340b0a2d0f6d4d39b52df9c30534548f4150
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-12 00:52:01 -0700

    add comment db

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
+// User model for MongoDB
 const mongoose = require('mongoose');
-
+// Create a schema for the User model
+// This schema defines the structure of the user document in the MongoDB collection
+// The schema includes fields for name, email, password, otp, otpExpiry, and IsVerified
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true},
